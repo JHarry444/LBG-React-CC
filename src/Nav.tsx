46fbb0870a3b524e634926515f8ddcc77cbf1ebc
JHarry <41322826+JHarry444@users.noreply.github.com>
2024-12-04 12:18:26 +0000

    Delete src/Nav.tsx

--- src/Nav.tsx
+++
@@ -1,9 +0,0 @@
-const Nav = ({children}) => {
-    return (<nav className="navbar"> {children} </nav>)
-}
-
-Nav.Link = ({children, href}) => {
-    return (
-        <a className="nav-item" href={href}>{children}</a>
-    );
-}
